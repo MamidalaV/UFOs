61194933dbd5af5c7e4fd0beff3c364b12594ce1
MamidalaV <74985818+MamidalaV@users.noreply.github.com>
2021-05-05 02:25:13 -0400

    adding updated files

--- static/js/app.js
+++ static/js/app.js
@@ -10,10 +10,8 @@ function buildTable(data){
 
     // Next, loop through each object in the data and append a row and cells for each value in the row
     data.forEach((dataRow) => { //arrow function to add data into tables
-
         // Append a row to the table body
-        let row = tobody.append("tr");
-        
+        let row = tbody.append("tr");
         // Loop through each field in the dataRow and add each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
@@ -21,28 +19,28 @@ function buildTable(data){
             }
         );
     });
-};
+}
 
-function handleClick(){
+function handleClick() {
     // Grab the datetime value from the filter
-    let date = d3.select('#datetime').property('value');
-    let filterData = tableData;
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
 
     // Check to see if a date was entered and filter the data using that date.
     if (date){
 
         // Apply `filter` to the table data to only keep the
         // rows where the `datetime` value matches the filter value
-        filteredData = filterData.filter(row => row.datetime === data);
+        filteredData = filteredData.filter(row => row.datetime === date);
     };
 
     // Rebuild the table using the filtered data
     // @NOTE: If no date was entered, then filteredData will just be the original tableData.
     buildTable(filteredData);
-};
+}
 
 // Attach an event to listen for the form button
-d3.selectAll("filter-btn").on("click",handleClick);
+d3.selectAll("#filter-btn").on("click",handleClick);
 
 // Build the table when the page loads
 buildTable(tableData);
